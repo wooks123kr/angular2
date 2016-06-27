b1995fedcfb94b3189e84e5c628d7bd07a0c24e9
Junwoo Kim <junwoo.kim@gmail.com>
2016-06-27 21:34:44 +0900

    5th commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,86 +1,49 @@
 import { Component } from '@angular/core';
-import {OnInit} from '@angular/core';
-import {Hero } from './hero';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
+
 import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from './dashboard.component';
+
 @Component({
   selector: 'my-app',
   template: `
       <h1>{{title}}</h1>
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="let hero of heroes"
-          [class.selected]="hero === selectedHero"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+      <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+      </nav>
+      <router-outlet></router-outlet>
     `,
-  styles: [`
-        .selected {
-          background-color: #CFD8DC !important;
-          color: white;
-        }
-        .heroes {
-          margin: 0 0 2em 0;
-          list-style-type: none;
-          padding: 0;
-          width: 15em;
-        }
-        .heroes li {
-          cursor: pointer;
-          position: relative;
-          left: 0;
-          background-color: #EEE;
-          margin: .5em;
-          padding: .3em 0;
-          height: 1.6em;
-          border-radius: 4px;
-        }
-        .heroes li.selected:hover {
-          background-color: #BBD8DC !important;
-          color: white;
-        }
-        .heroes li:hover {
-          color: #607D8B;
-          background-color: #DDD;
-          left: 1em;
-        }
-        .heroes .text {
-          position: relative;
-          top: -3px;
-        }
-        .heroes .badge {
-          display: inline-block;
-          font-size: small;
-          color: white;
-          padding: 0.8em 0.7em 0 0.7em;
-          background-color: #607D8B;
-          line-height: 1em;
-          position: relative;
-          left: -1px;
-          top: -4px;
-          height: 1.8em;
-          margin-right: .8em;
-          border-radius: 4px 0 0 4px;
-        }
-      `],
-  directives: [HeroDetailComponent],
-  providers: [HeroService]
+  directives: [ROUTER_DIRECTIVES],
+  styleUrls: ['app/app.component.css'],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
 })
-export class AppComponent implements OnInit{
-  ngOnInit() {
-    this.getHeroes();
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
   }
-  getHeroes(){
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-  }
-  constructor(private heroService: HeroService){
-  }
+])
+
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
